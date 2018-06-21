6f0c86d4be8f3bd8e2fbf68f7d90b174ec2c187b
fedorov <fedorov@krista.ru>
2018-06-21 14:59:15 +0300

    Мигрировал проект на .netcore2.  Сделал асинхронными запросы к серверу.

--- src/SuggestClient.cs
+++ src/SuggestClient.cs
@@ -1,91 +1,101 @@
 ﻿using System;
 using System.Net;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using RestSharp;
 
-namespace suggestionscsharp {
+namespace suggestionscsharp
+{
+    public class SuggestClient
+    {
+        private const string SUGGESTIONS_URL = "{0}/suggest";
+        private const string ADDRESS_RESOURCE = "address";
+        private const string PARTY_RESOURCE = "party";
+        private const string BANK_RESOURCE = "bank";
+        private const string FIO_RESOURCE = "fio";
+        private const string EMAIL_RESOURCE = "email";
 
-    public class SuggestClient {
-        const string SUGGESTIONS_URL = "{0}/suggest";
-        const string ADDRESS_RESOURCE = "address";
-        const string PARTY_RESOURCE = "party";
-        const string BANK_RESOURCE = "bank";
-        const string FIO_RESOURCE = "fio";
-        const string EMAIL_RESOURCE = "email";
+        private RestClient client;
+        private string token;
+        private ContentType contentType = ContentType.JSON;
 
-        RestClient client;
-        string token;
-        ContentType contentType = ContentType.JSON;
-
-        public IWebProxy Proxy {
+        public IWebProxy Proxy
+        {
             get { return client.Proxy; }
             set { client.Proxy = value; }
         }
 
-        static SuggestClient() {
-            // use SSL v3
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-        }
-
-        public SuggestClient(string token, string baseUrl) {
+        public SuggestClient(string token, string baseUrl)
+        {
             this.token = token;
-            this.client = new RestClient (String.Format (SUGGESTIONS_URL, baseUrl));
+            this.client = new RestClient(String.Format(SUGGESTIONS_URL, baseUrl));
         }
 
-        public SuggestAddressResponse QueryAddress(string address) {
+        public Task<SuggestAddressResponse> QueryAddress(string address)
+        {
             return QueryAddress(new AddressSuggestQuery(address));
         }
 
-        public SuggestAddressResponse QueryAddress(AddressSuggestQuery query) {
+        public Task<SuggestAddressResponse> QueryAddress(AddressSuggestQuery query)
+        {
             var request = new RestRequest(ADDRESS_RESOURCE, Method.POST);
             return Execute<SuggestAddressResponse>(request, query);
         }
 
-        public SuggestBankResponse QueryBank(string bank) {
+        public Task<SuggestBankResponse> QueryBank(string bank)
+        {
             return QueryBank(new BankSuggestQuery(bank));
         }
 
-        public SuggestBankResponse QueryBank(BankSuggestQuery query) {
+        public Task<SuggestBankResponse> QueryBank(BankSuggestQuery query)
+        {
             var request = new RestRequest(BANK_RESOURCE, Method.POST);
             return Execute<SuggestBankResponse>(request, query);
         }
 
-        public SuggestEmailResponse QueryEmail(string email) {
+        public Task<SuggestEmailResponse> QueryEmail(string email)
+        {
             var request = new RestRequest(EMAIL_RESOURCE, Method.POST);
             var query = new SuggestQuery(email);
             return Execute<SuggestEmailResponse>(request, query);
         }
 
-        public SuggestFioResponse QueryFio(string fio) {
+        public Task<SuggestFioResponse> QueryFio(string fio)
+        {
             return QueryFio(new FioSuggestQuery(fio));
         }
 
-        public SuggestFioResponse QueryFio(FioSuggestQuery query) {
+        public Task<SuggestFioResponse> QueryFio(FioSuggestQuery query)
+        {
             var request = new RestRequest(FIO_RESOURCE, Method.POST);
             return Execute<SuggestFioResponse>(request, query);
         }
 
-        public SuggestPartyResponse QueryParty(string party) {
+        public Task<SuggestPartyResponse> QueryParty(string party)
+        {
             return QueryParty(new PartySuggestQuery(party));
         }
 
-        public SuggestPartyResponse QueryParty(PartySuggestQuery query) {
+        public Task<SuggestPartyResponse> QueryParty(PartySuggestQuery query)
+        {
             var request = new RestRequest(PARTY_RESOURCE, Method.POST);
             return Execute<SuggestPartyResponse>(request, query);
         }
 
-        private T Execute<T>(RestRequest request, SuggestQuery query) where T : new() {
+        private async Task<T> Execute<T>(RestRequest request, SuggestQuery query) where T : new()
+        {
             request.AddHeader("Authorization", "Token " + this.token);
             request.AddHeader("Content-Type", contentType.Name);
             request.AddHeader("Accept", contentType.Name);
             request.RequestFormat = contentType.Format;
             request.XmlSerializer.ContentType = contentType.Name;
             request.AddBody(query);
-            var response = client.Execute<T>(request);
+            var response = await client.ExecuteTaskAsync<T>(request);
 
-            if (response.ErrorException != null) {
+            if (response.ErrorException != null)
+            {
                 throw response.ErrorException;
             }
+
             return response.Data;
         }
     }
--- src/SuggestClientTest.cs
+++ src/SuggestClientTest.cs
@@ -1,158 +1,167 @@
-﻿using NUnit.Framework;
-using RestSharp;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using Xunit;
 
-namespace suggestionscsharp {
-
-    [TestFixture]
-    public class SuggestionsClientTest {
-
+namespace suggestionscsharp
+{
+    public class SuggestionsClientTest
+    {
         public SuggestClient api { get; set; }
 
-        [SetUp]
-        public void SetUp() {
+        public SuggestionsClientTest()
+        {
             var token = Environment.GetEnvironmentVariable("DADATA_API_KEY");
             var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs";
             this.api = new SuggestClient(token, url);
         }
 
-        [Test]
-        public void SuggestAddressTest() {
+        [Fact]
+        public async void SuggestAddressTest()
+        {
             var query = "москва турчанинов 6с2";
-            var response = api.QueryAddress(query);
+            var response = await api.QueryAddress(query);
             var address_data = response.suggestions[0].data;
-            Assert.AreEqual("119034", address_data.postal_code);
-            Assert.AreEqual("7704", address_data.tax_office);
-            Assert.AreEqual("Кропоткинская", address_data.metro[0].name);
+            Assert.Equal("119034", address_data.postal_code);
+            Assert.Equal("7704", address_data.tax_office);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestAddressLocationsKladrTest() {
+        [Fact]
+        public async void SuggestAddressLocationsKladrTest()
+        {
             var query = new AddressSuggestQuery("ватутина");
             var location = new AddressData();
             location.kladr_id = "65";
             query.locations = new AddressData[] { location };
-            var response = api.QueryAddress(query);
-            Assert.AreEqual("693022", response.suggestions[0].data.postal_code);
+            var response = await api.QueryAddress(query);
+            Assert.Equal("693022", response.suggestions[0].data.postal_code);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestAddressLocationsFiasCityTest() {
+        [Fact]
+        public async void SuggestAddressLocationsFiasCityTest()
+        {
             var query = new AddressSuggestQuery("ватутина");
             var location = new AddressData();
             location.city_fias_id = "44388ad0-06aa-49b0-bbf9-1704629d1d68"; // Южно-Сахалинск
             query.locations = new AddressData[] { location };
-            var response = api.QueryAddress(query);
-            Assert.AreEqual("693022", response.suggestions[0].data.postal_code);
+            var response = await api.QueryAddress(query);
+            Assert.Equal("693022", response.suggestions[0].data.postal_code);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestAddressBoundsTest() {
+        [Fact]
+        public async void SuggestAddressBoundsTest()
+        {
             var query = new AddressSuggestQuery("ново");
             query.from_bound = new AddressBound("city");
             query.to_bound = new AddressBound("city");
-            var response = api.QueryAddress(query);
-            Assert.AreEqual("Новосибирск", response.suggestions[0].data.city);
+            var response = await api.QueryAddress(query);
+            Assert.Equal("Новосибирск", response.suggestions[0].data.city);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestAddressHistoryTest() {
+        [Fact]
+        public async void SuggestAddressHistoryTest()
+        {
             var query = "москва хабар";
-            var response = api.QueryAddress(query);
+            var response = await api.QueryAddress(query);
             var address_data = response.suggestions[0].data;
-            Assert.AreEqual("ул Черненко", address_data.history_values[0]);
+            Assert.Equal("ул Черненко", address_data.history_values[0]);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestBankTest() {
+        [Fact]
+        public async void SuggestBankTest()
+        {
             var query = "сбербанк";
-            var response = api.QueryBank(query);
-            Assert.AreEqual("044525225", response.suggestions[0].data.bic);
-            Assert.AreEqual("Москва", response.suggestions[0].data.address.data.city);
+            var response = await api.QueryBank(query);
+            Assert.Equal("044525225", response.suggestions[0].data.bic);
+            Assert.Equal("Москва", response.suggestions[0].data.address.data.city);
             Console.WriteLine(response.suggestions[0].data.opf.type);
             Console.WriteLine(response.suggestions[0].data.state.status);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestBankStatusTest() {
+        [Fact]
+        public async void SuggestBankStatusTest()
+        {
             var query = new BankSuggestQuery("витас");
             query.status = new PartyStatus[] { PartyStatus.LIQUIDATED };
-            var response = api.QueryBank(query);
-            Assert.AreEqual("044585398", response.suggestions[0].data.bic);
+            var response = await api.QueryBank(query);
+            Assert.Equal("044585398", response.suggestions[0].data.bic);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestBankTypeTest() {
+        [Fact]
+        public async void SuggestBankTypeTest()
+        {
             var query = new BankSuggestQuery("я");
             query.type = new BankType[] { BankType.NKO };
-            var response = api.QueryBank(query);
-            Assert.AreEqual("044525444", response.suggestions[0].data.bic);
+            var response = await api.QueryBank(query);
+            Assert.Equal("044525444", response.suggestions[0].data.bic);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestEmailTest() {
+        [Fact]
+        public async void SuggestEmailTest()
+        {
             var query = "anton@m";
-            var response = api.QueryEmail(query);
-            Assert.AreEqual("anton@mail.ru", response.suggestions[0].value);
+            var response = await api.QueryEmail(query);
+            Assert.Equal("anton@mail.ru", response.suggestions[0].value);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestFioTest() {
+        [Fact]
+        public async void SuggestFioTest()
+        {
             var query = "викт";
-            var response = api.QueryFio(query);
-            Assert.AreEqual("Виктор", response.suggestions[0].data.name);
+            var response = await api.QueryFio(query);
+            Assert.Equal("Виктор", response.suggestions[0].data.name);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestFioPartsTest() {
+        [Fact]
+        public async void SuggestFioPartsTest()
+        {
             var query = new FioSuggestQuery("викт");
-            query.parts = new FioPart [] { FioPart.SURNAME };
-            var response = api.QueryFio(query);
-            Assert.AreEqual("Викторова", response.suggestions[0].data.surname);
-            Console.WriteLine(string.Join ("\n", response.suggestions));
+            query.parts = new FioPart[] { FioPart.SURNAME };
+            var response = await api.QueryFio(query);
+            Assert.Equal("Викторова", response.suggestions[0].data.surname);
+            Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestPartyTest() {
+        [Fact]
+        public async void SuggestPartyTest()
+        {
             var query = "7707083893";
-            var response = api.QueryParty(query);
+            var response = await api.QueryParty(query);
             var party = response.suggestions[0];
             var address = response.suggestions[0].data.address;
-            Assert.AreEqual("7707083893", party.data.inn);
-            Assert.AreEqual("г Москва, ул Вавилова, д 19", address.value);
-            Assert.AreEqual("117997 ГОРОД МОСКВА, УЛИЦА ВАВИЛОВА, дом 19", address.data.source);
-            Assert.AreEqual("117312", address.data.postal_code);
+            Assert.Equal("7707083893", party.data.inn);
+            Assert.Equal("г Москва, ул Вавилова, д 19", address.value);
+            Assert.Equal("117997, ГОРОД МОСКВА, УЛИЦА ВАВИЛОВА, 19", address.data.source);
+            Assert.Equal("117312", address.data.postal_code);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestPartyStatusTest() {
+        [Fact]
+        public async void SuggestPartyStatusTest()
+        {
             var query = new PartySuggestQuery("витас");
             query.status = new PartyStatus[] { PartyStatus.LIQUIDATED };
-            var response = api.QueryParty(query);
-            Assert.AreEqual("4713008497", response.suggestions[0].data.inn);
+            var response = await api.QueryParty(query);
+            Assert.Equal("4713008497", response.suggestions[0].data.inn);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
 
-        [Test]
-        public void SuggestPartyTypeTest() {
+        [Fact]
+        public async void SuggestPartyTypeTest()
+        {
             var query = new PartySuggestQuery("витас");
             query.type = PartyType.INDIVIDUAL;
-            var response = api.QueryParty(query);
-            Assert.AreEqual("773165008890", response.suggestions[0].data.inn);
+            var response = await api.QueryParty(query);
+            Assert.Equal("470411980055", response.suggestions[0].data.inn);
             Console.WriteLine(string.Join("\n", response.suggestions));
         }
     }
 }
-
